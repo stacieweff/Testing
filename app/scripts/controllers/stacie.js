f54605f74717d06ca2df455568663e8b6eb67d60
weffelms <stacie.weffelmeyer@wwt.com>
2016-07-26 10:12:15 -0500

    karma

---
+++ app/scripts/controllers/stacie.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testingApp.controller:StacieCtrl
+ * @description
+ * # StacieCtrl
+ * Controller of the testingApp
+ */
+angular.module('testingApp')
+  .controller('StacieCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
